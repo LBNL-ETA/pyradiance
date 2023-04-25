4ee5ff6a5e99e0c1c19ea0967d77947b8b231d81
Taoning Wang <taoning@gmail.com>
2023-04-25 09:31:07 -0700

    bump version 0.0.8a1 to 0.0.9a1

--- setup.py
+++ setup.py
@@ -474,7 +474,7 @@ setup(
     name="pyradiance",
     author="LBNL",
     author_email="taoningwang@lbl.gov",
-    version="0.0.8a1",
+    version="0.0.9a1",
     description="Python interface for Radiance command-line tools",
     long_description=Path("README.md").read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
