c29cf1e9c4a44de2bb50c70edbe0413afabf3acb
Taoning Wang <taoning@gmail.com>
2023-04-25 09:27:02 -0700

    feat(cal): add rcalc

--- pyradiance/cal.py
+++ pyradiance/cal.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import subprocess as sp
 from typing import Optional
 
-from .aux import BINPATH, handle_called_process_error
+from .anci import BINPATH, handle_called_process_error
 
 
 @handle_called_process_error
@@ -33,6 +33,62 @@ def cnt(
         cmd.append("-s")
     cmd.extend([str(d) for d in dims])
     return sp.run(cmd, check=True, stdout=sp.PIPE).stdout
+
+
+@handle_called_process_error
+def rcalc(
+    inp, 
+    sep=None, 
+    inform=None, 
+    incount=1, 
+    outform=None, 
+    passive=False, 
+    pass_negative=False, 
+    inlimit=None,
+    outlimit=None,
+    source=None,
+    expr=None,
+    assign=None,
+    exact_only=False,
+    ignore_newlines=False,
+    silent=False,
+):
+    stdin = None
+    cmd = [str(BINPATH / "rcalc")]
+    if sep is not None:
+        cmd.append(f'-t{sep}')
+    if inform is not None:
+        cmd.append(f'-i{inform}{incount}')
+    if outform is not None:
+        cmd.append(f'-o{outform}')
+    if inform and outform:
+        if passive:
+            cmd.append('-p')
+        elif pass_negative:
+            cmd.append('-P')
+    if inlimit is not None:
+        cmd.extend(['-in', str(inlimit)])
+    if outlimit is not None:
+        cmd.extend(['-on', str(outlimit)])
+    if source is not None:
+        cmd.extend(['-f', source])
+    if expr is not None:
+        cmd.extend(['-e', expr])
+    if assign is not None:
+        cmd.extend(['-s', assign])
+    if exact_only:
+        cmd.append('-b')
+    if ignore_newlines:
+        cmd.append('-l')
+    if silent:
+        cmd.append('-w')
+    if isinstance(inp, (Path, str)):
+        cmd.append(str(inp))
+    elif isinstance(inp, bytes):
+        stdin = inp
+    else:
+        raise TypeError(f"inp must be Path, str or bytes, not {type(inp)}")
+    return sp.run(cmd, check=True, capture_output=True, input=stdin).stdout
 
 
 @handle_called_process_error
@@ -101,7 +157,7 @@ def total(
     if outlimit:
         cmd.extend(["-on", str(outlimit)])
     if sep:
-        cmd.extend(f"-t{sep}")
+        cmd.append(f"-t{sep}")
     stdin = None
     if isinstance(inp, bytes):
         stdin = inp
