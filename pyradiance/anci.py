6579621c2e078612a6e5c3276ad038698aa1b437
Sarith Subramaniam <sarith@sarith.in>
2023-08-16 11:26:38 +0200

    feat(anci.py): Added a utility write method to write data to file.

--- pyradiance/anci.py
+++ pyradiance/anci.py
@@ -5,6 +5,7 @@ Auxiliary functions.
 from pathlib import Path
 from functools import wraps
 from subprocess import CalledProcessError
+import os
 
 BINPATH = Path(__file__).parent / "bin"
 
@@ -27,3 +28,27 @@ def handle_called_process_error(func):
             return result
 
     return wrapper
+
+def write(
+        file_path,
+        data,
+        overwrite=True,
+        mode='wb') -> str:
+    """
+    Write data to a file.
+    Args:
+        file_path: path to file
+        data: data to write
+        overwrite: if True, overwrite the file if it already exists
+        mode: 'w' for text, 'wb' for binary
+
+    Returns:
+        str: path to file
+    """
+    if not overwrite and os.path.exists(file_path):
+        raise Exception(f'The path {file_path} already exists and '
+                        f'"overwrite" has been set to False.')
+    else:
+        with open(file_path, mode=mode) as write_data:
+            write_data.write(data)
+    return file_path
