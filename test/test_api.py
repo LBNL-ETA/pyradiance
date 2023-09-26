8faf6bd2b87c786cf3639fe8415e118f3e62cffc
Taoning Wang <taoning@gmail.com>
2023-09-26 10:29:24 -0700

    update tests

--- test/test_api.py
+++ test/test_api.py
@@ -3,22 +3,22 @@ Test pyradiance api
 """
 from datetime import datetime
 from pathlib import Path
-import os
 
 import pyradiance as pr
 from pyradiance import model, param
 import pytest
 
 
-
 @pytest.fixture
-def resdir():
-    return Path(os.path.dirname(os.path.abspath(__file__))) / "Resources"
+def resources_dir():
+    return (Path(__file__).parent / "Resources").absolute()
+
 
 def test_primitive():
     prim = pr.Primitive("void", "plastic", "white", [], [0.5, 0.6, 0.7, 0, 0])
     assert prim.ptype == "plastic"
     assert prim.bytes == b"void plastic white 0 0 5 0.5 0.6 0.7 0 0 "
+
 
 def test_install():
     radiance_version = pr.rtrace(None, None, version=True)
@@ -27,60 +27,66 @@ def test_install():
 
 def test_gendaylit():
     res = pr.gendaylit(datetime(2022, 2, 1, 12), 37, 122, 120, dirnorm=800, diffhor=100)
-    assert res != b""
+    ptypes = {p.ptype for p in pr.parse_primitive(res.decode())}
+    assert ptypes == {"light", "brightfunc", "source"}
 
 
-def test_load_scene(resdir: Path):
+def test_load_scene(resources_dir: Path):
     """Test the load_scene function."""
     # assert load_scene('test') == 'test'
     scene = pr.Scene("test_scene")
-    scene.add_surface(str(resdir / "floor.rad"))
-    scene.add_surface(str(resdir / "ceiling.rad"))
-    scene.add_material(str(resdir / "materials.mat"))
-    scene.add_source(str(resdir / "sky.rad"))
+    scene.add_surface(str(resources_dir / "floor.rad"))
+    scene.add_surface(str(resources_dir / "ceiling.rad"))
+    scene.add_material(str(resources_dir / "materials.mat"))
+    scene.add_source(str(resources_dir / "sky.rad"))
     assert scene.sid == "test_scene"
-    assert str(resdir / "floor.rad") in scene.surfaces
-    assert str(resdir / "ceiling.rad") in scene.surfaces
-    assert str(resdir / "materials.mat") in scene.materials
-    assert str(resdir / "sky.rad") in scene.sources
+    assert str(resources_dir / "floor.rad") in scene.surfaces
+    assert str(resources_dir / "ceiling.rad") in scene.surfaces
+    assert str(resources_dir / "materials.mat") in scene.materials
+    assert str(resources_dir / "sky.rad") in scene.sources
 
 
-def test_render(resdir: Path):
+def test_render(resources_dir: Path):
     """Test the render function."""
     aview = pr.View(
-        position = (1, 1, 1),
-        direction = (0, -1, 0),
+        position=(1, 1, 1),
+        direction=(0, -1, 0),
     )
     scene = pr.Scene("test_scene")
-    scene.add_surface(resdir / "floor.rad")
-    scene.add_surface(resdir / "ceiling.rad")
-    scene.add_material(resdir / "materials.mat")
-    scene.add_source(resdir / "sky.rad")
+    scene.add_surface(resources_dir / "floor.rad")
+    scene.add_surface(resources_dir / "ceiling.rad")
+    scene.add_material(resources_dir / "materials.mat")
+    scene.add_source(resources_dir / "sky.rad")
     scene.add_view(aview)
     pr.render(scene)
 
 
-def test_rfluxmtx(resdir: Path):
+def test_rfluxmtx(resources_dir: Path):
     """Test the rfluxmtx function."""
-    receiver = resdir / "skyr4.rad"
+    receiver = resources_dir / "skyr4.rad"
     rays = b"1.0 1 1 0 0 1"
-    scene = (resdir / "materials.mat", resdir / "floor.rad", resdir / "ceiling.rad")
+    scene = (
+        resources_dir / "materials.mat",
+        resources_dir / "floor.rad",
+        resources_dir / "ceiling.rad",
+    )
     result = pr.rfluxmtx(receiver, rays=rays, scene=scene)
     assert len(result) > 0
 
 
-def test_read_rad(resdir: Path):
+def test_read_rad(resources_dir: Path):
     """Test the read_rad function."""
     result = pr.read_rad(
-        str(resdir / "materials.mat"),
-        str(resdir / "floor.rad"),
-        str(resdir / "ceiling.rad")
+        str(resources_dir / "materials.mat"),
+        str(resources_dir / "floor.rad"),
+        str(resources_dir / "ceiling.rad"),
     )
     assert len(result) > 0
     assert isinstance(result[0], pr.Primitive)
 
-def test_get_view_resolu(resdir: Path ):
-    view, res = pr.get_view_resolu(str(resdir/ "test.hdr"))
+
+def test_get_view_resolu(resources_dir: Path):
+    view, res = pr.get_view_resolu(str(resources_dir / "test.hdr"))
     assert view.vtype == "a"
     assert res.orient == "-Y+X"
     assert res.xr == 544
@@ -88,15 +94,17 @@ def test_get_view_resolu(resdir: Path ):
     assert view.horiz == 180
     assert view.vert == 180
 
-def test_BSDF(resdir: Path):
+
+def test_BSDF(resources_dir: Path):
     """Test the bsdf function."""
-    with pr.BSDF(str(resdir / "t3.xml")) as bsdf:
+    with pr.BSDF(str(resources_dir / "t3.xml")) as bsdf:
         _t = bsdf.direct_hemi(30, 0, "t")
         assert pytest.approx(_t, 0.0001) == 7.6456e-2
         _a = bsdf.size(30, 0)
         assert pytest.approx(_a[0], 0.0001) == 7.6699e-4
         _sv = bsdf.eval(0, 0, 180, 0)
         assert pytest.approx(_sv[1], 0.001) == 4.997
+
 
 def test_parse_view():
     inp_str = "-vta -vv 180 -vh 180 -vp 0 0 0 -vd 0 -1 0"
@@ -122,9 +130,9 @@ def test_parse_opt():
     assert res == answer
 
 
-def test_ra_tiff(tmp_path: Path, resdir: Path):
+def test_ra_tiff(tmpdir: Path, resources_dir: Path):
     """Test the ra_tiff function."""
-    hdr = resdir / "test.hdr"
-    out = tmp_path / "test.tif"
+    hdr = resources_dir / "test.hdr"
+    out = tmpdir / "test.tif"
     pr.ra_tiff(hdr, out=str(out))
     assert out.exists()
