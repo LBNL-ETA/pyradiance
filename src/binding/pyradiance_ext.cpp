65f6ea32c9e49a4ffdd8e3a9dd43f4fb037487a3
taoning <taoningw@gmail.com>
2024-11-18 17:38:01 -0800

    update

--- src/binding/pyradiance_ext.cpp
+++ src/binding/pyradiance_ext.cpp
@@ -1,176 +1,147 @@
 #include "RtraceSimulManager.h"
+#include "RcontribSimulManager.h"
 #include "otspecial.h"
 #include "otypes.h"
 #include "platform.h"
 #include "ray.h"
 #include "resolu.h"
 #include "source.h"
+#include <functional>
+#include <memory>
 #include <nanobind/nanobind.h>
 #include <nanobind/ndarray.h>
+#include <unordered_map>
+#include <utility>
 #include <vector>
 
 namespace nb = nanobind;
 
-using namespace nb::literals;
+// Store callbacks globally (not ideal but works for now)
+static std::unordered_map<void *, std::shared_ptr<nb::callable>>
+    stored_callbacks;
 
-struct CallbackData {
-  nb::object callback;
-  nb::object data;
-};
+// Function that will be called from C++
+int callback_wrapper(RAY *r, void *cd) {
+  auto it = stored_callbacks.find(cd);
+  if (it == stored_callbacks.end())
+    return -1;
 
-struct PyRtraceSimulManager : public RtraceSimulManager {
-
-private:
-  CallbackData cooked_cb_data;
-  CallbackData trace_cb_data;
-  PyRtraceSimulManager(nb::object cb = nb::none(), nb::object cd = nb::none(),
-                       const char *octn = nullptr)
-      : RtraceSimulManager(nullptr, nullptr, octn) {
-    if (!cb.is_none())
-      SetCookedCall(cb, cd);
+  nb::gil_scoped_acquire acquire;
+  try {
+    nb::object result = (*(it->second))(nb::cast(r), nb::cast(cd));
+    return nb::cast<int>(result);
+  } catch (const std::exception &e) {
+    return -1;
   }
-
-public:
-  static int PythonCallback(RAY *r, void *cd) {
-    CallbackData *cb_data = static_cast<CallbackData *>(cd);
-    if (cb_data->callback.is_none())
-      return -1;
-    nb::object py_ray = nb::cast(r, nb::rv_policy::reference);
-    try {
-      nb::object result;
-      if (!cb_data->data.is_none()) {
-        result = cb_data->callback(py_ray, cb_data->data);
-      } else {
-        result = cb_data->callback(py_ray);
-      }
-      return nb::cast<int>(result);
-    } catch (nb::python_error &e) {
-      // Handle exception if needed
-      e.restore();
-      return -1;
-    }
-  }
-  void SetCookedCall(nb::object cb, nb::object cd = nb::none()) {
-    // Store the Python callable and data
-    cooked_cb_data.callback = cb;
-    cooked_cb_data.data = cd;
-    // Set the C++ callback function to our static method
-    RtraceSimulManager::SetCookedCall(&PyRtraceSimulManager::PythonCallback,
-                                      &cooked_cb_data);
-  }
-
-  void SetTraceCall(nb::object cb, nb::object cd = nb::none()) {
-    trace_cb_data.callback = cb;
-    trace_cb_data.data = cd;
-    RtraceSimulManager::SetTraceCall(&PyRtraceSimulManager::PythonCallback,
-                                     &trace_cb_data);
-  }
-};
+}
 
 NB_MODULE(radiance_ext, m) {
 
   m.doc() = "Radiance extension";
-
   nb::class_<RAY>(m, "RAY")
       .def(nb::init<>())
-      .def_ro("rorg", &RAY::rorg)
-      /*.def_property(*/
-      /*    "rorg",*/
-      /*    [](RAY &self) {*/
-      /*      return std::array<RREAL, 3>{self.rorg[0], self.rorg[1],*/
-      /*                                  self.rorg[2]};*/
-      /*    },*/
-      /*    [](RAY &self, const std::array<RREAL, 3> &value) {*/
-      /*      self.rorg[0] = value[0];*/
-      /*      self.rorg[1] = value[1];*/
-      /*      self.rorg[2] = value[2];*/
-      /*    })*/
-      .def_ro("rdir", &RAY::rdir)
-      .def_ro("rmax", &RAY::rmax)
-      .def_ro("rot", &RAY::rot)
-      .def_ro("rop", &RAY::rop)
-      .def_ro("ron", &RAY::ron)
-      .def_ro("rod", &RAY::rod)
-      .def_ro("uv", &RAY::uv)
-      .def_ro("pert", &RAY::pert)
-      .def_ro("rmt", &RAY::rmt)
-      .def_ro("rxt", &RAY::rxt)
-      .def_ro("parent", &RAY::parent)
-      .def_ro("rno", &RAY::rno)
-      .def_ro("robj", &RAY::robj)
-      .def_ro("rsrc", &RAY::rsrc)
-      .def_ro("rweight", &RAY::rweight)
-      .def_ro("gecc", &RAY::gecc)
-      .def_ro("rcoef", &RAY::rcoef)
-      .def_ro("pcol", &RAY::pcol)
-      .def_ro("mcol", &RAY::mcol)
-      .def_ro("rcol", &RAY::rcol)
-      .def_ro("cext", &RAY::cext)
-      .def_ro("albedo", &RAY::albedo)
-      .def_ro("rflips", &RAY::rflips)
-      .def_ro("rlvl", &RAY::rlvl)
-      .def_ro("rtype", &RAY::rtype)
-      .def_ro("crtype", &RAY::crtype);
+      .def_prop_ro("rorg",
+                   [](const RAY &r) -> nb::tuple {
+                     return nb::make_tuple(r.rorg[0], r.rorg[1], r.rorg[2]);
+                   })
+      .def_prop_ro("rdir",
+                   [](const RAY &r) -> nb::tuple {
+                     return nb::make_tuple(r.rdir[0], r.rdir[1], r.rdir[2]);
+                   })
+      .def_prop_ro("rop",
+                   [](const RAY &r) -> nb::tuple {
+                     return nb::make_tuple(r.rop[0], r.rop[1], r.rop[2]);
+                   })
+      .def_prop_ro("ron",
+                   [](const RAY &r) -> nb::tuple {
+                     return nb::make_tuple(r.ron[0], r.ron[1], r.ron[2]);
+                   })
+      .def_prop_ro("pert",
+                   [](const RAY &r) -> nb::tuple {
+                     return nb::make_tuple(r.pert[0], r.pert[1], r.pert[2]);
+                   })
+      .def_prop_ro("rmax", [](const RAY &r) { return r.rmax; })
+      .def_prop_ro("rod", [](const RAY &r) { return r.rod; })
+      .def_prop_ro("rweight", [](const RAY &r) { return r.rweight; })
+      .def_prop_ro("rno", [](const RAY &r) { return r.rno; })
+      .def_prop_ro("rtype", [](const RAY &r) { return r.rtype; })
+      .def_prop_ro("mcol",
+                   [](const RAY &r) -> nb::tuple {
+                     return nb::make_tuple(r.mcol[0], r.mcol[1], r.mcol[2]);
+                   })
+      .def_prop_ro("rcol", [](const RAY &r) -> nb::tuple {
+        return nb::make_tuple(r.rcol[0], r.rcol[1], r.rcol[2]);
+      });
 
-  nb::class_<RadSimulManager>(m, "RadSimulManager")
-      .def(nb::init<const char *>())
-      .def("load_octree", &RadSimulManager::LoadOctree)
-      .def("new_header", &RadSimulManager::NewHeader)
-      .def("add_header",
-           nb::overload_cast<const char *>(&RadSimulManager::AddHeader),
-           "Add a line to header (adds newline if none)")
-      .def("add_header",
-           [](RadSimulManager &self, const std::vector<const char *> &args) {
-             return self.AddHeader(args.size(),
-                                   const_cast<char **>(args.data()));
-           })
-      .def("get_head_len", &RadSimulManager::GetHeadLen)
-      .def("get_head_str", (const char *(RadSimulManager::*)() const) &
-                               RadSimulManager::GetHeadStr)
-      .def("get_head_str",
-           (const char *(RadSimulManager::*)(const char *, bool) const) &
-               RadSimulManager::GetHeadStr)
-      .def("nthreads", &RadSimulManager::NThreads)
-      .def("set_thread_count", &RadSimulManager::SetThreadCount,
-           nb::arg("nt") = 0)
-      .def("threads_available", &RadSimulManager::ThreadsAvailable)
-      .def("ready", &RadSimulManager::Ready)
-      .def("process_ray", &RadSimulManager::ProcessRay)
-      .def("wait_result", &RadSimulManager::WaitResult)
-      .def("cleanup", &RadSimulManager::Cleanup, nb::arg("everything") = false);
-
-  nb::class_<PyRtraceSimulManager, RtraceSimulManager>(m, "RtraceSimulManager")
+  nb::class_<RtraceSimulManager>(m, "RtraceSimulManager")
 
       .def(nb::init<>())
-      .def(nb::init<RayReportCall *, void *, const char *>())
+      .def("load_octree", &RtraceSimulManager::LoadOctree)
       .def("set_thread_count", &RtraceSimulManager::SetThreadCount,
            nb::arg("nt") = 0)
       .def(
           "enqueue_bundle",
-          [](RtraceSimulManager &self,
-             nb::ndarray<double, nb::shape<-1, 3>> orig_direc,
+          [](RtraceSimulManager &self, const nb::sequence &orig_direc,
              RNUMBER rID0 = 0) {
-            // Ensure array has correct dimensions
-            if (orig_direc.ndim() != 2 || orig_direc.shape(1) != 3) {
-              throw std::runtime_error("Input array must be of shape (n, 3)");
+            std::vector<std::array<double, 3>> data;
+            auto seq = nb::cast<nb::sequence>(orig_direc);
+            size_t n = len(seq) / 2;
+            data.reserve(n * 2);
+            for (size_t i = 0; i < n * 2; ++i) {
+              auto row = nb::cast<nb::sequence>(seq[i]);
+              std::array<double, 3> point;
+              for (size_t j = 0; j < 3; ++j) {
+                point[j] = nb::cast<double>(row[j]);
+              }
+              data.push_back(point);
             }
-            int n = orig_direc.shape(0);
-            // Access data pointer
+
+            return self.EnqueueBundle(
+                reinterpret_cast<const double(*)[3]>(data.data()), n, rID0);
+          },
+          nb::arg("orig_direc"), nb::arg("rID0") = 0)
+      .def(
+          "enqueue_bundle_array",
+          [](RtraceSimulManager &self,
+             const nb::ndarray<double, nb::shape<-1, 3>> &orig_direc,
+             RNUMBER rID0 = 0) {
+            std::vector<std::array<double, 3>> data;
+            /*auto arr = nb::cast<nb::ndarray<double>>(orig_direc);*/
+            if (orig_direc.ndim() != 2 || orig_direc.shape(1) != 3) {
+              throw std::runtime_error("NumPy array must be of shape (n, 3)");
+            }
+            int n = orig_direc.shape(0) / 2;
             const double(*orig_direc_ptr)[3] =
                 reinterpret_cast<const double(*)[3]>(orig_direc.data());
             return self.EnqueueBundle(orig_direc_ptr, n, rID0);
           },
           nb::arg("orig_direc"), nb::arg("rID0") = 0)
-      .def("enqueue_ray", &RtraceSimulManager::EnqueueRay, nb::arg("org"),
-           nb::arg("dir"), nb::arg("rID") = 0)
-      .def("set_cooked_call", &PyRtraceSimulManager::SetCookedCall,
-           nb::arg("callback"))
-      .def("set_trace_call", &PyRtraceSimulManager::SetTraceCall,
-           nb::arg("callback"))
       .def("ready", &RtraceSimulManager::Ready)
       .def("flush_queue", &RtraceSimulManager::FlushQueue)
       .def("cleanup", &RtraceSimulManager::Cleanup,
-           nb::arg("everything") = false);
+           nb::arg("everything") = false)
+      .def_rw("rt_flags", &RtraceSimulManager::rtFlags)
+      .def("set_cooked_call",
+           [](RtraceSimulManager &self, nb::callable callback) {
+             auto cb_ptr = std::make_shared<nb::callable>(std::move(callback));
+             void *key = cb_ptr.get();
+             stored_callbacks[key] = cb_ptr;
+
+             self.SetCookedCall(callback_wrapper, key);
+           })
+      .def("set_trace_call",
+           [](RtraceSimulManager &self, nb::callable callback) {
+             auto cb_ptr = std::make_shared<nb::callable>(std::move(callback));
+             void *key = cb_ptr.get();
+             stored_callbacks[key] = cb_ptr;
+
+             self.SetTraceCall(callback_wrapper, key);
+           })
+      .def("cleanup_callbacks", [](RtraceSimulManager &self) {
+        stored_callbacks.clear();
+        self.SetCookedCall(nullptr, nullptr);
+        self.SetTraceCall(nullptr, nullptr);
+      });
 
   nb::enum_<decltype(RTdoFIFO)>(m, "RTFlags")
       .value("RTdoFIFO", RTdoFIFO)
@@ -178,4 +149,37 @@ NB_MODULE(radiance_ext, m) {
       .value("RTlimDist", RTlimDist)
       .value("RTimmIrrad", RTimmIrrad)
       .value("RTmask", RTmask);
+
+  // For the callback handling
+  m.def("make_ray_report_callback", [](nb::callable callback) {
+    return [callback](RAY *r, void *cd) -> int {
+      nb::gil_scoped_acquire acquire;
+      try {
+        nb::object result = callback(nb::cast(r), nb::cast(cd));
+        return nb::cast<int>(result);
+      } catch (const std::exception &e) {
+        return -1;
+      }
+    };
+  });
+
+  nb::class_<RcontribOutput>(m, "RcontribOutput")
+      .def(nb::init<const char *>(), nb::arg("fnm") = nullptr)
+      .def("GetName", &RcontribOutput::GetName)
+      .def("SetRowsDone", &RcontribOutput::SetRowsDone)
+      .def("GetRow", &RcontribOutput::GetRow)
+      .def("InsertionP", &RcontribOutput::InsertionP)
+      .def("DoneRow", &RcontribOutput::DoneRow)
+      .def("Next",
+           static_cast<const RcontribOutput *(RcontribOutput::*)() const>(
+               &RcontribOutput::Next))
+      .def("Next", static_cast<RcontribOutput *(RcontribOutput::*)()>(
+                       &RcontribOutput::Next))
+      .def_ro("rData", &RcontribOutput::rData)
+      .def_ro("rowBytes", &RcontribOutput::rowBytes)
+      .def_ro("omod", &RcontribOutput::omod)
+      .def_ro("obin", &RcontribOutput::obin)
+      .def_ro("begData", &RcontribOutput::begData)
+      .def_ro("curRow", &RcontribOutput::curRow)
+      .def_ro("nRows", &RcontribOutput::nRows);
 }
--- tests/test_bind.py
+++ tests/test_bind.py
@@ -1,31 +1,57 @@
 import pyradiance as pr
 
+
+def cooked_callback(ray, client_data):
+    print("\nCooked callback called!")
+    try:
+        # Access ray properties directly
+        print(f"Ray origin: {ray.rorg}")
+        print(f"Ray direction: {ray.rdir}")
+        print(f"Ray max distance: {ray.rmax}")
+        print(f"Ray weight: {ray.rweight}")
+        print(f"Ray number: {ray.rno}")
+        print(f"Ray type: {ray.rtype}")
+        print(f"Radiance {ray.rcol}")
+    except Exception as e:
+        print(f"Error in cooked callback: {e}")
+        import traceback
+
+        traceback.print_exc()
+    return 0
+
+
+def trace_callback(ray, client_data):
+    print("\nTrace callback called!")
+    try:
+        print(f"Ray origin: {ray.rorg}")
+        print(f"Ray direction: {ray.rdir}")
+        print(f"Ray max distance: {ray.rmax}")
+        print(f"Ray weight: {ray.rweight}")
+        print(f"Ray number: {ray.rno}")
+        print(f"Ray type: {ray.rtype}")
+        print(f"Radiance {ray.rcol}")
+    except Exception as e:
+        print(f"Error in trace callback: {e}")
+        import traceback
+
+        traceback.print_exc()
+    return 0
+
+
+rays = [
+    [1.0, 2.0, 3.0],
+    [0.0, 0.0, 1.0],  # Ray 1 origin and direction
+    [4.0, 5.0, 6.0],
+    [0.0, 1.0, 0.0],  # Ray 2 origin and direction
+]
+
 mgr = pr.RtraceSimulManager()
-
 mgr.load_octree("test.oct")
-
-print(mgr.set_thread_count(1))
-
-
-def cooked_callback(ray):
-    # Process the ray
-    return 0  # Return appropriate integer
-
-def trace_callback(ray):
-    # Process the trace
-    return 0  # Return appropriate integer
-
-
-# Set callbacks
+mgr.set_thread_count(1)
 mgr.set_cooked_call(cooked_callback)
 mgr.set_trace_call(trace_callback)
-
-# Enqueue rays
-orig_direc = [
-    [1.0, 2.0, 3.0, 0.0, 0.0, 1.0],  # Ray 1 origin and direction
-    [4.0, 5.0, 6.0, 0.0, 1.0, 0.0],  # Ray 2 origin and direction
-]
-mgr.EnqueueBundle(orig_direc)
-
-# Start processing
-mgr.FlushQueue()
+mgr.rt_flags = pr.RTFlags.RTdoFIFO.value
+mgr.enqueue_bundle(rays)
+mgr.flush_queue()
+mgr.cleanup(True)
+del mgr
