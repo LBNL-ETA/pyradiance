2267143e2b46c0e70c7f32aecd5281ba5fadcfe3
taoning <taoningw@gmail.com>
2024-12-31 20:43:29 -0800

    use unittest instead of pytest

--- tests/test_rtrace.py
+++ tests/test_rtrace.py
@@ -5,46 +5,50 @@ import numpy as np
 
 import pyradiance as pr
 
+result = []
+
+
+def cooked_callback(ray, client_data):
+    print("\nCooked callback called!")
+    try:
+        print(f"Ray origin: {ray.rorg}")
+        print(f"Ray direction: {ray.rdir}")
+        print(f"Ray max distance: {ray.rmax}")
+        print(f"Ray weight: {ray.rweight}")
+        print(f"Ray number: {ray.rno}")
+        print(f"Ray type: {ray.rtype}")
+        print(f"Radiance {ray.rcol}")
+        result.append(ray.rcol)
+    except Exception as e:
+        print(f"Error in cooked callback: {e}")
+        import traceback
+
+        traceback.print_exc()
+    return 0
+
+
+def trace_callback(ray, client_data):
+    print("\nTrace callback called!")
+    try:
+        print(f"Ray origin: {ray.rorg}")
+        print(f"Ray direction: {ray.rdir}")
+        print(f"Ray max distance: {ray.rmax}")
+        print(f"Ray weight: {ray.rweight}")
+        print(f"Ray number: {ray.rno}")
+        print(f"Ray type: {ray.rtype}")
+        print(f"Radiance {ray.rcol}")
+        result.append(ray.rcol)
+    except Exception as e:
+        print(f"Error in trace callback: {e}")
+        import traceback
+
+        traceback.print_exc()
+    return 0
+
 
 class TestRtraceSimulManager(unittest.TestCase):
 
     octree = os.path.join(os.path.dirname(__file__), "Resources", "trace.oct")
-
-    @staticmethod
-    def cooked_callback(ray, client_data):
-        print("\nCooked callback called!")
-        try:
-            print(f"Ray origin: {ray.rorg}")
-            print(f"Ray direction: {ray.rdir}")
-            print(f"Ray max distance: {ray.rmax}")
-            print(f"Ray weight: {ray.rweight}")
-            print(f"Ray number: {ray.rno}")
-            print(f"Ray type: {ray.rtype}")
-            print(f"Radiance {ray.rcol}")
-        except Exception as e:
-            print(f"Error in cooked callback: {e}")
-            import traceback
-
-            traceback.print_exc()
-        return 0
-
-    @staticmethod
-    def trace_callback(ray, client_data):
-        print("\nTrace callback called!")
-        try:
-            print(f"Ray origin: {ray.rorg}")
-            print(f"Ray direction: {ray.rdir}")
-            print(f"Ray max distance: {ray.rmax}")
-            print(f"Ray weight: {ray.rweight}")
-            print(f"Ray number: {ray.rno}")
-            print(f"Ray type: {ray.rtype}")
-            print(f"Radiance {ray.rcol}")
-        except Exception as e:
-            print(f"Error in trace callback: {e}")
-            import traceback
-
-            traceback.print_exc()
-        return 0
 
     def test_rtrace(self):
         rays = np.array(
@@ -56,15 +60,16 @@ class TestRtraceSimulManager(unittest.TestCase):
             ]
         )
         rparam = pr.get_ray_params()
-        rparam.ab = 1
+        rparam.ab = 0
         pr.set_ray_params(rparam)
         mgr = pr.RtraceSimulManager()
         mgr.load_octree(self.octree)
-        mgr.set_thread_count(2)
-        mgr.set_cooked_call(self.cooked_callback)
-        # mgr.set_trace_call(trace_callback)
+        mgr.set_thread_count(1)
+        mgr.set_cooked_call(cooked_callback)
+        mgr.set_trace_call(trace_callback)
         mgr.rt_flags = pr.RTdoFIFO
         mgr.enqueue_bundle(rays)
         mgr.flush_queue()
         mgr.cleanup(True)
         del mgr
+        self.assertEqual(len(result), 4)
