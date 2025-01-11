94bfe839f7faa23914247665078681011a9f930c
Taoning Wang <taoning@f6-2e-52-a7-71-5a.dhcp.lbl.gov>
2025-01-10 16:41:54 -0800

    manually fix version.c

--- src/radiance/rt/Version.c
+++ src/radiance/rt/Version.c
@@ -1,4 +1,4 @@
 /*
  * This file was created automatically during build.
  */
-char VersionID[]="RADIANCE 6.0a lastmod Wed Nov 13 10:13:10 AM PST 2024 by taoningw on lbnl";
+char VersionID[]="RADIANCE 6.0a";
